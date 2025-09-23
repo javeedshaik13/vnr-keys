97d1f99068c3fe850963843d4cbf99b4a3be22f6
rishithreddy89 <rishithreddygopidi1@gmail.com>
2025-09-23 17:44:52 +0530

    Update config.js

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -121,7 +121,6 @@ export const getCorsOrigins = () => {
       'http://127.0.0.1:3204',
       'http://127.0.0.1:3205',
       'http://127.0.0.1:5173',
-      'https://vnr-keys.vercel.app/'
       'https://vnr-keys.vercel.app',
       'https://vnr-keys-1.onrender.com'
     ],
@@ -129,9 +128,9 @@ export const getCorsOrigins = () => {
       'http://localhost:3203', // For local development against dev backend
       'http://localhost:3204',
       'http://localhost:5173',
-      'https://vnr-keys.vercel.app/'
       'https://vnr-keys.vercel.app',
       'https://vnr-keys-1.onrender.com'
+      
     ],
     pro: []
   };
